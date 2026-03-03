3230c5e7a85fbbfba12de4efd1967bfb8748634e
mdkgameteam <guarimulti76@gmail.com>
2026-03-03 00:28:11 -0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     // ensure third-party icons are pre-bundled for faster dev start
